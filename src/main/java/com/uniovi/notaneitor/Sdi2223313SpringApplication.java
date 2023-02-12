9e6cf23e22de14944e63cdb1e63953e1877f4ed6
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-02-12 20:49:31 +0100

    SDI-2223-313-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/notaneitor/Sdi2223313SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaneitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223313SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223313SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/notaneitor/Sdi2223313SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaneitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223313SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
