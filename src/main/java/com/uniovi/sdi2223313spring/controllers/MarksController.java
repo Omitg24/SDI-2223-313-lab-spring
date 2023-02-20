8c9568005fc2020a5c5b437ee57c3b2e32d2533c
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-02-20 22:40:42 +0100

    SDI-2223-313-3.1-Thymeleaf, fragmentos.

--- src/main/java/com/uniovi/sdi2223313spring/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi2223313spring/controllers/MarksController.java
@@ -54,4 +54,10 @@ public class MarksController {
         model.addAttribute("mark", marksService.getMark(id));
         return "mark/edit";
     }
+
+    @RequestMapping("/mark/list/update")
+    public String updateList(Model model){
+        model.addAttribute("markList", marksService.getMarks() );
+        return "mark/list :: tableMarks";
+    }
 }
