4060e9ada1018f34533facf36a5d9349f4037914
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-02-21 01:19:24 +0100

    SDI-2223-313-3.5-Validación de datos en el servidor.

--- src/main/java/com/uniovi/sdi2223313spring/controllers/UsersController.java
+++ src/main/java/com/uniovi/sdi2223313spring/controllers/UsersController.java
@@ -3,11 +3,14 @@ package com.uniovi.sdi2223313spring.controllers;
 import com.uniovi.sdi2223313spring.entities.User;
 import com.uniovi.sdi2223313spring.services.SecurityService;
 import com.uniovi.sdi2223313spring.services.UsersService;
+import com.uniovi.sdi2223313spring.validators.SignUpFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,9 @@ public class UsersController {
 
     @Autowired
     private SecurityService securityService;
+
+    @Autowired
+    private SignUpFormValidator signUpFormValidator;
 
     @RequestMapping("/user/list")
     public String getListado(Model model) {
@@ -65,10 +71,21 @@ public class UsersController {
     }
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    public String signup(@ModelAttribute("user") User user, Model model) {
+    public String signup(@Validated User user, BindingResult result) {
+        signUpFormValidator.validate(user, result);
+        if (result.hasErrors()) {
+            return "signup";
+        }
+
         usersService.addUser(user);
         securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
         return "redirect:home";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
--- src/main/java/com/uniovi/sdi2223313spring/services/UsersService.java
+++ src/main/java/com/uniovi/sdi2223313spring/services/UsersService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
