716a30c425f2ddc61dddc1b55a989eef47d5ef13
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-03-05 19:16:14 +0100

    SDI-2223-313-5.4-Casos Uso VistaHome.

--- src/test/java/com/uniovi/sdi2223313spring/Sdi2223313SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2223313spring/Sdi2223313SpringApplicationTests.java
@@ -1,9 +1,14 @@
 package com.uniovi.sdi2223313spring;
 
+import com.uniovi.sdi2223313spring.pageobjects.PO_HomeView;
+import com.uniovi.sdi2223313spring.pageobjects.PO_Properties;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -51,51 +56,36 @@ class Sdi2223313SpringApplicationTests {
 
     @Test
     @Order(1)
-    void PR01() {
+    void PR01A() {
+        PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
     }
-
     @Test
     @Order(2)
-    void PR02() {
+    void PR01B() {
+        List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver,
+                PO_Properties.getSPANISH());
+        Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
+                PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
     }
 
+    //PR02. Opción de navegación. Pinchar en el enlace Registro en la página home
     @Test
     @Order(3)
-    void PR03() {
+    public void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
     }
-
+    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
     @Test
     @Order(4)
-    void PR04() {
+    public void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
     }
 
+    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
     @Test
     @Order(5)
-    void PR05() {
-    }
-
-    @Test
-    @Order(6)
-    void PR06() {
-    }
-
-    @Test
-    @Order(7)
-    void PR07() {
-    }
-
-    @Test
-    @Order(8)
-    void PR08() {
-    }
-
-    @Test
-    @Order(9)
-    void PR09() {
-    }
-
-    @Test
-    @Order(10)
-    void PR010() {
+    public void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
+                PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
     }
 }
